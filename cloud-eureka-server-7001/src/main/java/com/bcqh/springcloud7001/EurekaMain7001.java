6cccc6ed5b6db06ae36ef866dc1f1d6b09bc19ad
bcqhyzq <3308889271@qq.com>
2020-06-29 21:00:43 +0800

    spring-cloud Eureka注册中心

---
+++ cloud-eureka-server-7001/src/main/java/com/bcqh/springcloud7001/EurekaMain7001.java
@@ -0,0 +1,19 @@
+package com.bcqh.springcloud7001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Description:
+ *
+ * @author yzq
+ * @date 2020/6/26
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
